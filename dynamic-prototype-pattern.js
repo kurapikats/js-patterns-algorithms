fc18cc0fcc09f445031b03c7fa9db74deeb30104
kurapikats <kurapikats@yahoo.com>
2017-12-25 21:59:38 +0800

    add dynamic prototype pattern

---
+++ dynamic-prototype-pattern.js
@@ -0,0 +1,18 @@
+// use function expression, es2015 fat arrow won't work here
+const Person = function (name, age, location) {
+  this.name = name
+  this.age = age
+  this.location = location
+
+  if (typeof this.printPerson !== 'function') {
+    Person.prototype.printPerson = () => {
+      console.log(`Name: ${this.name}, Age: ${this.age}, Location: ${this.location}`)
+    }
+  }
+}
+
+// we need to use 'new' keyword, this behaves like a class constructor
+const onin = new Person('Onin', 35, 'Makati')
+const marebel = new Person('Marebel', 34, 'Roxas')
+onin.printPerson()
+marebel.printPerson()
