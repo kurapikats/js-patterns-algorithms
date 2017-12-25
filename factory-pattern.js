90107ec3547c7ce72bc5906d8aaae7d4dae1c2eb
kurapikats <kurapikats@yahoo.com>
2017-12-25 21:19:30 +0800

    update factory pattern to es2015+

--- factory-pattern.js
+++ factory-pattern.js
@@ -1,17 +1,19 @@
-var Person = function (name, age, location) {
-	var data = {}
-  data.name = name
-  data.age = age
-  data.location = location
+const Person = (name, age, location) => {
+	const data = {
+    name,
+    age,
+    location
+  }
 
-  data.printPerson = function () {
+  data.printPerson = () => {
   	console.log('Name: ' + data.name + ', Age: ' + data.age + ', Location: ' +  data.location)
   }
 
   return data
 }
 
-var person1 = Person('Onin', 35, 'Makati')
-var person2 = Person('Marebel', 34, 'Roxas')
-person1.printPerson()
-person2.printPerson()
+// factory pattern doesn't use 'new' keyword
+const onin = Person('Onin', 35, 'Makati')
+const marebel = Person('Marebel', 34, 'Roxas')
+onin.printPerson()
+marebel.printPerson()
