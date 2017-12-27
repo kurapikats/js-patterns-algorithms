73c9c970c5faaafcff16185ee2b609699ea77fe8
kurapikats <kurapikats@yahoo.com>
2017-12-27 08:59:44 +0800

    add iterators

---
+++ iterators.js
@@ -0,0 +1,9 @@
+const numbers = [1,2,3,4]
+
+const iterator = numbers[Symbol.iterator]()
+
+console.dir(iterator.next())
+console.dir(iterator.next())
+console.dir(iterator.next())
+console.dir(iterator.next())
+console.dir(iterator.next())
