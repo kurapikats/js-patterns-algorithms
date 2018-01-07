bb3ee3e70875fa6c78d3dff00cd63e5028ada99b
kurapikats <kurapikats@yahoo.com>
2018-01-07 09:35:39 +0800

    add recursion

---
+++ recursion.js
@@ -0,0 +1,12 @@
+// decompose the problem to small subset
+// and repeat calling it
+var factorial = function fac(num) {
+  if (num === 1) {
+    return 1;
+  }
+
+  return num * fac(num - 1);
+}
+
+var final = factorial(5)
+console.log(final)
