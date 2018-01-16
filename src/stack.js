2d53b8229d7bfc57d5869683cd3c1a3c8b838450
kurapikats <kurapikats@yahoo.com>
2018-01-16 23:32:37 +0000

    add stack data structure

---
+++ src/stack.js
@@ -0,0 +1,48 @@
+// basic implementation of Stack Data Structure
+function Stack() {
+  items = []
+
+  this.push = (element) => {
+    items.push(element)
+  }
+
+  this.pop = () => {
+    return items.pop()
+  }
+
+  this.peek = () => {
+    return items[items.length - 1]
+  }
+
+  this.isEmpty = () => {
+    return items.length === 0
+  }
+
+  this.size = () => {
+    return items.length
+  }
+
+  this.clear = () => {
+    items = []
+  }
+
+  this.print = () => {
+    console.log(items.toString())
+  }
+}
+
+/*
+//tests
+let stack = new Stack()
+console.log(stack.isEmpty())
+stack.push('onin')
+stack.push('bot')
+stack.push('test')
+console.log(stack.size())
+stack.pop()
+console.log(stack.size())
+stack.print()
+console.log(stack.peek())
+stack.clear()
+console.log(stack.size())
+*/
